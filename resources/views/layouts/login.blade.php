afbc06638d8a519b511224b0e3897f9bfee1c672
Juan Dlc <daner_10@live.com.mx>
2026-02-11 19:16:03 -0600

    v1.1.1.1.9

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -2,34 +2,51 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>@yield('titulo')</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <title>@yield('titulo')</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('NiceAdmin/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('NiceAdmin/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('NiceAdmin/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('NiceAdmin/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-  <link href="{{ asset('NiceAdmin/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-  <link href="{{ asset('NiceAdmin/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-  <link href="{{ asset('NiceAdmin/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('images/icons/favicon.ico') }}" />
 
-  <!-- Template Main CSS File -->
-  <link href="{{ asset('NiceAdmin/assets/css/style.css') }}" rel="stylesheet">
+    <!-- Bootstrap (solo una vez) -->
+    <link href="{{ asset('NiceAdmin/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
-  <!-- =======================================================
+    <!-- Iconos -->
+    <link href="{{ asset('NiceAdmin/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}" rel="stylesheet">
+
+    <!-- Plugins -->
+    <link href="{{ asset('NiceAdmin/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/animate/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/css-hamburgers/hamburgers.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/select2/select2.min.css') }}" rel="stylesheet">
+
+    <!-- Estilos propios -->
+    <link href="{{ asset('css/util.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('NiceAdmin/assets/css/style.css') }}" rel="stylesheet">
+
+    <!-- =======================================================
   * Template Name: NiceAdmin
   * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
   * Updated: Apr 20 2024 with Bootstrap v5.3.3
@@ -39,23 +56,46 @@
 </head>
 
 <body>
-  
-  @yield('contenido')
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    @yield('contenido')
 
-  <!-- Vendor JS Files -->
-  <script src="{{ asset('NiceAdmin/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-  <script src="{{ asset('NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('NiceAdmin/assets/vendor/chart.js/chart.umd.js') }}"></script>
-  <script src="{{ asset('NiceAdmin/assets/vendor/echarts/echarts.min.js') }}"></script>
-  <script src="{{ asset('NiceAdmin/assets/vendor/quill/quill.js') }}"></script>
-  <script src="{{ asset('NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-  <script src="{{ asset('NiceAdmin/assets/vendor/tinymce/tinymce.min.js') }}"></script>
-  <script src="{{ asset('NiceAdmin/assets/vendor/php-email-form/validate.js') }}"></script>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Template Main JS File -->
-  <script src="{{ asset('NiceAdmin/assets/js/main.js') }}"></script>
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('NiceAdmin/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/quill/quill.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/php-email-form/validate.js') }}"></script>
+    <!-- jQuery -->
+    <script src="{{ asset('vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+
+    <!-- Bootstrap -->
+    <script src="{{ asset('vendor/bootstrap/js/popper.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+
+    <!-- Select2 -->
+    <script src="{{ asset('vendor/select2/select2.min.js') }}"></script>
+
+    <!-- Tilt JS -->
+    <script src="{{ asset('vendor/tilt/tilt.jquery.min.js') }}"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            $('.js-tilt').tilt({
+                scale: 1.1
+            });
+        });
+    </script>
+
+    <!-- Scripts propios -->
+    <script src="{{ asset('js/main.js') }}"></script>
+
+    <!-- Template Main JS File -->
+    <script src="{{ asset('NiceAdmin/assets/js/main.js') }}"></script>
 
 </body>
 
--- resources/views/modules/auth/login.blade.php
+++ resources/views/modules/auth/login.blade.php
@@ -1,126 +1,142 @@
 @extends('layouts.login')
 
 @section('titulo', $titulo)
+
 @section('contenido')
-    <main>
-        <div class="container">
-
-            <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-
-                            <div class="d-flex justify-content-center py-4">
-                                <a href="#" class="logo d-flex align-items-center w-auto">
-                                    <img src="assets/img/logo.png" alt="">
-                                    <span class="d-none d-lg-block">SISTEMA DE EVIDENCIAS</span>
-                                </a>
-                            </div><!-- End Logo -->
-
-                            <div class="card mb-3">
-
-                                <div class="card-body">
-
-                                    <div class="pt-4 pb-2 text-center">
-                                        <h5
-                                            class="card-title pb-0 fs-4 d-flex justify-content-center align-items-center gap-2">
-                                            LOGIN DE USUARIOS
-
-                                            <button type="button" class="btn btn-sm btn-outline-secondary rounded-circle"
-                                                data-bs-toggle="modal" data-bs-target="#manualLogin" title="Ver manual">
-                                                ?
-                                            </button>
-                                        </h5>
-
-                                        <p class="small">Ingresa tu correo y contraseña para acceder</p>
-                                    </div>
-
-                                    <form class="row g-3 needs-validation" novalidate method="POST"
-                                        action="{{ route('logear') }}">
-                                        @csrf
-                                        <div class="col-12">
-                                            <label for="email" class="form-label">Email</label>
-                                            <div class="input-group has-validation">
-                                                <input type="text" name="email" class="form-control" id="email"
-                                                    required>
-                                                <div class="invalid-feedback">Ingresa tu correo</div>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-12">
-                                            <label for="password" class="form-label">Password</label>
-                                            <input type="password" name="password" class="form-control" id="password"
-                                                required>
-                                            <div class="invalid-feedback">Escribe tu contraseña</div>
-                                        </div>
-
-                                        <div class="col-12">
-                                            <button class="btn btn-primary w-100" type="submit">Login</button>
-                                        </div>
-                                    </form>
-                                    <!--validacion que viene de logear-->
-                                    <div>
-                                        @if ($errors->any())
-                                        @endif
-                                        <p>
-                                        <ul>
-                                            @foreach ($errors->all() as $error)
-                                                <li>{{ $error }}</li>
-                                            @endforeach
-                                        </ul>
-                                        </p>
-
-                                    </div>
-
-                                </div>
-                            </div>
-
-                            <div class="credits">
-                                Designed by <a target="_blank" href="https://github.com/ezekingzote">EZEQUIEL</a> WITH <a
-                                    target="_blank" href="https://github.com/JuanDlc10">JUAN </a>
-                            </div>
-
-                        </div>
+<main>
+    <section class="min-vh-100 d-flex align-items-center justify-content-center">
+        <div class="limiter w-100">
+            <div class="container-login100">
+                <div class="wrap-login100 d-flex align-items-center justify-content-center">
+                    <!-- Imagen -->
+                    <div class="login100-pic js-tilt" data-tilt>
+                        <img src="{{ asset('Login/img-01.png') }}" alt="Login Image">
                     </div>
-                </div>
-                <!-- Modal Manual Login -->
-                <div class="modal fade" id="manualLogin" tabindex="-1" aria-labelledby="manualLoginLabel"
-                    aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-body">
 
-                            <div class="text-center mb-3">
-                                <img src="{{ asset('Manual/login-step1.png') }}" class="img-fluid rounded shadow-sm"
-                                    alt="Paso 1 Login">
-                            </div>
+                    <!-- Formulario -->
+                    <form class="login100-form validate-form" method="POST" action="{{ route('logear') }}">
+                        @csrf
 
-                            <div class="alert alert-primary">
-                                <strong>Paso 1:</strong> Escribe el correo asignado por el administrador.
-                            </div>
+                        <span class="login100-form-title">
+                            LOGIN DE USUARIO
+                        </span>
 
-                            <div class="text-center mb-3">
-                                <img src="{{ asset('Manual/login-step2.png') }}" class="img-fluid rounded shadow-sm"
-                                    alt="Paso 2 Login">
-                            </div>
-
-                            <div class="alert alert-primary">
-                                <strong>Paso 2:</strong> Ingresa tu contraseña.
-                            </div>
-
-                            <div class="text-center">
-                                <img src="{{ asset('Manual/login-step3.png') }}" class="img-fluid rounded shadow-sm"
-                                    alt="Paso 3 Login">
-                            </div>
-
-                            <div class="alert alert-success mt-3">
-                                <strong>Paso 3:</strong> Haz clic en el botón Login.
-                            </div>
-
+                        <!-- Email -->
+                        <div class="wrap-input100 validate-input"
+                             data-validate="Valid email is required: ex@abc.xyz">
+                            <input class="input100" 
+                                   type="text" 
+                                   name="email" 
+                                   placeholder="Email"
+                                   value="{{ old('email') }}">
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                            </span>
                         </div>
-                    </div>
+
+                        <!-- Password -->
+                        <div class="wrap-input100 validate-input"
+                             data-validate="Password is required">
+                            <input class="input100" 
+                                   type="password" 
+                                   name="password"
+                                   placeholder="Password">
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
+                                <i class="fa fa-lock" aria-hidden="true"></i>
+                            </span>
+                        </div>
+
+                        <!-- Botón -->
+                        <div class="container-login100-form-btn">
+                            <button class="login100-form-btn" type="submit">
+                                Login
+                            </button>
+                        </div>
+
+                        <!-- Errores -->
+                        @if ($errors->any())
+                            <div class="alert alert-danger mt-3">
+                                <ul class="mb-0">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <!-- Ayuda -->
+                        <div class="text-center p-t-12">
+                            <a href="#" 
+                               data-bs-toggle="modal" 
+                               data-bs-target="#manualLogin"
+                               class="txt2">
+                                ¿Necesitas ayuda?
+                            </a>
+                        </div>
+
+                    </form>
+
                 </div>
-            </section>
+            </div>
         </div>
-    </main><!-- End #main -->
+    </section>
+</main>
+
+
+<div class="modal fade" id="manualLogin" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content rounded-4 shadow-lg">
+
+            <!-- Header -->
+            <div class="modal-header border-0">
+                <h5 class="modal-title fw-bold">
+                    Manual de Inicio de Sesión
+                </h5>
+                <button type="button" 
+                        class="btn-close" 
+                        data-bs-dismiss="modal" 
+                        aria-label="Cerrar"></button>
+            </div>
+
+            <!-- Body -->
+            <div class="modal-body px-4 pb-4" style="max-height: 75vh; overflow-y: auto;">
+
+                <!-- Paso 1 -->
+                <div class="mb-4">
+                    <img src="{{ asset('Manual/login-step1.png') }}"
+                         class="img-fluid rounded shadow-sm w-100"
+                         alt="Paso 1 Login">
+                    <div class="alert alert-primary mt-3 text-center">
+                        <strong>Paso 1:</strong> Escribe tu correo electrónico.
+                    </div>
+                </div>
+
+                <!-- Paso 2 -->
+                <div class="mb-4">
+                    <img src="{{ asset('Manual/login-step2.png') }}"
+                         class="img-fluid rounded shadow-sm w-100"
+                         alt="Paso 2 Login">
+                    <div class="alert alert-primary mt-3 text-center">
+                        <strong>Paso 2:</strong> Ingresa tu contraseña.
+                    </div>
+                </div>
+
+                <!-- Paso 3 -->
+                <div class="mb-2">
+                    <img src="{{ asset('Manual/login-step3.png') }}"
+                         class="img-fluid rounded shadow-sm w-100"
+                         alt="Paso 3 Login">
+                    <div class="alert alert-success mt-3 text-center">
+                        <strong>Paso 3:</strong> Haz clic en el botón Login.
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
 
 @endsection
