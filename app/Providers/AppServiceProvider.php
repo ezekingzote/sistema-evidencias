11104dd1f2762857411fd0a819cf810819bb5727
ezekingzote <alejandromendoza020302@gmail.com>
2026-06-03 23:31:06 -0600

    v1.1.1.5.7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceScheme('https');
+        if (!app()->environment('local')) {
+            URL::forceScheme('https');
+        }
         //gates en Laravel
         Gate::define('ver-admin', function (User $user){
             return $user->rol === 'admin';
